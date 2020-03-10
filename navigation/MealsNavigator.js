bae005a3432af7c64fdee8b94fd680b45d5dd969
Carlos Avina <carlosavilu11@gmail.com>
2020-03-09 23:30:07 -0600

    basic navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,11 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealScreen from '../screens/CategoryMealScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
-const MealsNavigator = createSwitchNavigator({
+const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
     CategoryMeal: CategoryMealScreen,
     MealDetail: MealDetailScreen
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, Button, StyleSheet} from 'react-native';
 
 const CategoriesScreen = props => {
     return (
         <View style={styles.screen} >
             <Text>The Categories Screen</Text>
+            <Button title="Next screen" onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeal'});
+                // props.navigation.replace('CategoryMeal'); // Replaces the view in the stack, doesn't allow to navigate back
+            }}/>
         </View>
     );
 }
