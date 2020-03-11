6c8214ebab47533e1b5e6d2f9f3449720ee85fb7
Carlos Avina <carlos.avina@cbqasolutions.com>
2020-03-11 10:38:01 -0600

    dynamic navigation options

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet} from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from '../constants/Colors';
 
 const CategoryMealScreen = props => {
-    const catId = props.navigation.getParam('categoryId');
-    const category = CATEGORIES.find(cat => cat.id === catId);
+  const catId = props.navigation.getParam("categoryId");
+  const category = CATEGORIES.find(cat => cat.id === catId);
 
-    return (
-        <View style={styles.screen} >
-            <Text>The Category Meal Screen</Text>
-            <Text>{category.title}</Text>
-            <Button title="Next Screen" onPress={() => {
-                props.navigation.navigate({routeName: "MealDetail"});
-            }}/> 
-            <Button title="Go back" onPress={() => {
-                props.navigation.goBack();
-                // props.navigation.pop(); // Only works on stack navigators
-            }}/> 
-        </View>
-    );
-}
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen</Text>
+      <Text>{category.title}</Text>
+      <Button
+        title="Next Screen"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
+      <Button
+        title="Go back"
+        onPress={() => {
+          props.navigation.goBack();
+          // props.navigation.pop(); // Only works on stack navigators
+        }}
+      />
+    </View>
+  );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const category = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: category.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+  };
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
 export default CategoryMealScreen;
