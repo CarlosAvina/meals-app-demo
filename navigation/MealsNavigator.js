897b01e5b98565d5eef2aaa7564e4ef2d476cfd3
Carlos Avina <carlos.avina@cbqasolutions.com>
2020-03-18 09:32:50 -0600

    tabs navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,14 @@
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createBottomTab } from "react-navigation-bottom-tabs";
 
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -24,4 +27,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const BottomTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
+});
+
+export default createAppContainer(BottomTabNavigator);
