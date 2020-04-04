ef8e6fd125bb3aada13f932c22f7e526e3291080
Carlos Avina <carlos.avina@cbqasolutions.com>
2020-04-03 20:46:45 -0600

    bottom tab options

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,14 +1,15 @@
+import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-// import { createBottomTab } from "react-navigation-bottom-tabs";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
-import FavoritesScreen from '../screens/FavoritesScreen';
+import FavoritesScreen from "../screens/FavoritesScreen";
 
 const MealsNavigator = createStackNavigator(
   {
@@ -27,9 +28,42 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const BottomTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favorites: FavoritesScreen
-});
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={22}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons
+              name="ios-star"
+              size={22}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.primaryColor
+    }
+  }
+);
 
 export default createAppContainer(BottomTabNavigator);
